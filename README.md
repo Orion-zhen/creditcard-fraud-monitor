9514cd6e0055367a493cf93991bae2e5d3d858ab
orion <net4orion@163.com>
2023-11-13 19:53:17 +0800

    Add: ipex support

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # creditcard-fraud-monitor
-XJTU校企合作课程习题: 信用卡交易欺诈检测
+
+本项目基于[第 13 代英特尔® 酷睿™ i5 处理器 i5-13600K](https://www.intel.cn/content/www/cn/zh/products/sku/230493/intel-core-i513600k-processor-24m-cache-up-to-5-10-ghz/specifications.html)和[英特尔锐炫™ a 系列显卡 A770](https://www.intel.cn/content/www/cn/zh/products/sku/229151/intel-arc-a770-graphics-16gb/specifications.html)编写
--- main.py
+++ main.py
@@ -15,33 +15,39 @@ if __name__ == "__main__":
     else:
         data = pd.read_csv("creditcard.csv")
     
-    full_dataset = CreditCardDataset(data)
-    train_dataset, test_dataset = splitter(full_dataset, args.split)
-    input_size = len(full_dataset.features)
-    model = Classifier(input_size=input_size)
-    if args.device == "xpu" or args.device == "ipex":
-        try:
-            import intel_extension_for_pytorch as ipex
+    if args.method == "dl":
+    
+        full_dataset = CreditCardDataset(data)
+        train_dataset, test_dataset = splitter(full_dataset, args.split)
+        input_size = len(full_dataset.features)
+        model = Classifier(input_size=input_size)
 
-            model = ipex.optimize(model)
-        except:
-            raise ModuleNotFoundError("Please install ipex first")
+        best_loss = train_dl(
+            model=model,
+            device=args.device,
+            train_dataset=train_dataset,
+            epochs=args.epochs,
+            batch_size=args.batch_size,
+            lr=args.lr,
+            output_path=args.output_path,
+        )
 
-    best_loss = train_dl(
-        model=model,
-        device=args.device,
-        train_dataset=train_dataset,
-        epochs=args.epochs,
-        batch_size=args.batch_size,
-        lr=args.lr,
-        output_path=args.output_path,
-    )
+        # ipex优化仅在推理时有效
+        model.eval()
+        if args.ipex:
+            try:
+                import intel_extension_for_pytorch as ipex
 
-    accuracy = predict(
-        model=model,
-        device=args.device,
-        test_dataset=test_dataset,
-        batch_size=args.batch_size,
-        output_path=args.output_path,
-    )
-    print(f"Best loss: {best_loss}, Accuracy: {accuracy}")
+                model = ipex.optimize(model)
+            except:
+                print("ipex uncapable!")
+
+        accuracy = predict(
+            model=model,
+            device=args.device,
+            test_dataset=test_dataset,
+            batch_size=args.batch_size,
+            output_path=args.output_path,
+        )
+        print(f"Best loss: {best_loss}, Accuracy: {accuracy}")
+    
